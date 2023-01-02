8e0131cb196f0ae0fe9102cc0da323d58bf3bde3
iamtaylorward <taylor@electriqmarketing.com>
2023-01-01 16:55:34 -0800

    3js setup

---
+++ src/script.js
@@ -0,0 +1,116 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.SphereGeometry(15,32,16,0,6.283185307179586, 0, 1.5707964);
+geometry.parameters.thetaLength = 1.33831847042925
+console.log(geometry)
+
+
+const topHalfMaterial = new THREE.MeshBasicMaterial()
+const bottomHalfMaterial = new THREE.MeshBasicMaterial()
+topHalfMaterial.color = new THREE.Color(0xE8E9E8)
+bottomHalfMaterial.color = new THREE.Color(0xB5B5B5)
+
+// Mesh
+const topHalfSphere = new THREE.Mesh(geometry,topHalfMaterial)
+const bottomHalfSphere = new THREE.Mesh(geometry,bottomHalfMaterial)
+
+bottomHalfSphere.rotation.z =  Math.PI
+
+scene.add(topHalfSphere)
+scene.add(bottomHalfSphere)
+
+
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 69
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 40
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor( 0xffffff, 1 );
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // top.rotation.y = .5 * elapsedTime
+
+    // Update Orbital Controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
